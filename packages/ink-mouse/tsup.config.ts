4b43bd2da5acffc268b937ca4a799dff3ae4a85f
Maxim (neiromaster) Gavrilenko <parrysh@mail.ru>
2025-11-30 22:06:01 +0500

    feat(ink-mouse): Add initial ink-mouse package

---
+++ packages/ink-mouse/tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['esm', 'cjs'],
+  dts: true,
+  sourcemap: true,
+  minify: true,
+  target: 'node18',
+  outDir: 'dist',
+  clean: true,
+  bundle: true,
+});
